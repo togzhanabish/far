f839ffcc4cd1887244afd11a82eb9bc7aea8693c
Togzhan <togzhanabish369@gmail.com>
2017-03-11 13:19:41 +0600

    far manager

---
+++ Farmanager/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void showInfo(DirectoryInfo dir, int cursor)
+        {
+            Console.Clear();
+            FileSystemInfo[] infos = dir.GetFileSystemInfos();
+            for (int i = 0; i < infos.Length; i++)
+            {
+
+                if (i == cursor)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                }
+
+                if (infos[i].GetType() == typeof(DirectoryInfo))
+                {
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                }
+
+                Console.WriteLine(infos[i].Name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.CursorVisible = false;
+            int cursor = 0;
+            DirectoryInfo dir = new DirectoryInfo(@"c:\far2.0\informatics6");
+
+            while (true)
+            {
+                showInfo(dir, cursor);
+                ConsoleKeyInfo btnn = Console.ReadKey();
+                switch (btnn.Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        if (cursor > 0) cursor--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        if (cursor < dir.GetFileSystemInfos().Length - 1) cursor++;
+                        break;
+                    case ConsoleKey.Enter:
+                        FileSystemInfo fs = dir.GetFileSystemInfos()[cursor];
+                        if (fs.GetType() == typeof(DirectoryInfo))
+                        {
+                            dir = new DirectoryInfo(fs.FullName);
+                        }
+                        else
+                        {
+
+                            Console.Clear();
+                            StreamReader sr = new StreamReader(fs.FullName);
+                            string s = sr.ReadToEnd();
+                            Console.Write(s);
+
+                            sr.Close();
+                            Console.ReadKey();
+
+                        }
+                        break;
+                    case ConsoleKey.Escape:
+                        dir = dir.Parent;
+                        break;
+                }
+
+            }
+        }
+
+    }
+}
